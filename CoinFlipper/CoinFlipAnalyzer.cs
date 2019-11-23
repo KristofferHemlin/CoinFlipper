417ca0d77f7a2dbb5a45f8364fae15f70c230f26
CLAMON-8244LRQ\Kristoffer Hemlin <kristoffer.hemlin@claremont.se>
2019-11-23 10:26:07 +0100

    First solution

--- CoinFlipper/CoinFlipAnalyzer.cs
+++ CoinFlipper/CoinFlipAnalyzer.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using CoinFlipper.Models;
 using CoinFlipper.Hardcore.Adapters;
+using System.Linq;
+using System;
 
 namespace CoinFlipper
 {
@@ -9,7 +11,38 @@ namespace CoinFlipper
         public int CountLongestStreak(IEnumerable<CoinFlipResult> flips)
         {
             //return HardcoreAdapter.CountLongestStreak(flips);
-            return new LongestStreakObfuscatedRecursiveStrategy().Count(flips);
+            //return new LongestStreakObfuscatedRecursiveStrategy().Count(flips);
+
+            var list = flips.ToList();
+            foreach (var coins in list)
+            {
+                var stringCoins = Convert.ToString(coins);
+                Console.WriteLine(stringCoins);
+
+                if (stringCoins == "Head")
+                {
+                    currentHead++;
+
+                    currentTail = 0;
+                }
+                else
+                {
+                    currentTail++;
+                    currentHead = 0;
+                }
+                if (currentTail > resultTail)
+                    resultTail = currentTail;
+                else if (currentHead > resultHead)
+                    resultHead = currentHead;
+            }
+            if (resultHead > resultTail)
+                return resultHead;
+            else
+                return resultTail;
         }
+        public int resultHead { get; set; }
+        public int resultTail { get; set; }
+        public int currentHead { get; set; }
+        public int currentTail { get; set; }
     }
 }
