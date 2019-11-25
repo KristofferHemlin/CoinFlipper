fbf2c5708150620842b0cb4911ae85140115de3b
CLAMON-8244LRQ\Kristoffer Hemlin <kristoffer.hemlin@claremont.se>
2019-11-25 08:18:44 +0100

    New solution

--- CoinFlipper/CoinFlipAnalyzer.cs
+++ CoinFlipper/CoinFlipAnalyzer.cs
@@ -12,37 +12,58 @@ namespace CoinFlipper
         {
             //return HardcoreAdapter.CountLongestStreak(flips);
             //return new LongestStreakObfuscatedRecursiveStrategy().Count(flips);
+            string current = null;
+            int highest = 0;
+            int count = 0;
 
             var list = flips.ToList();
             foreach (var coins in list)
             {
-                var stringCoins = Convert.ToString(coins);
-                Console.WriteLine(stringCoins);
-
-                if (stringCoins == "Head")
+                string stringCoin = Convert.ToString(coins);
+                if (stringCoin == current)
                 {
-                    currentHead++;
-
-                    currentTail = 0;
+                    count++;
+                    if (count > highest)
+                        highest = count;
                 }
                 else
                 {
-                    currentTail++;
-                    currentHead = 0;
+                    count = 1;
+                    current = stringCoin;
+                    if (list.Count == 0)
+                        highest = 0;
+                    if (list.Count == 1)
+                        highest = 1;
                 }
-                if (currentTail > resultTail)
-                    resultTail = currentTail;
-                else if (currentHead > resultHead)
-                    resultHead = currentHead;
             }
-            if (resultHead > resultTail)
-                return resultHead;
-            else
-                return resultTail;
-        }
-        public int resultHead { get; set; }
-        public int resultTail { get; set; }
-        public int currentHead { get; set; }
-        public int currentTail { get; set; }
+            return highest;
+                //    var stringCoins = Convert.ToString(coins);
+                //    Console.WriteLine(stringCoins);
+
+                //    if (stringCoins == "Head")
+                //    {
+                //        currentHead++;
+
+                //        currentTail = 0;
+                //    }
+                //    else
+                //    {
+                //        currentTail++;
+                //        currentHead = 0;
+                //    }
+                //    if (currentTail > resultTail)
+                //        resultTail = currentTail;
+                //    else if (currentHead > resultHead)
+                //        resultHead = currentHead;
+                //}
+                //if (resultHead > resultTail)
+                //    return resultHead;
+                //else
+                //    return resultTail;
+            }
+        //public int resultHead { get; set; }
+        //public int resultTail { get; set; }
+        //public int currentHead { get; set; }
+        //public int currentTail { get; set; }
     }
 }
